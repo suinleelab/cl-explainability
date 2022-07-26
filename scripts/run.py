09033cc6e697735e3701c297af03b5a03c2a4fed
Chris Lin <clin25@cs.washington.edu>
2022-07-26 15:56:25 -0700

    Add a script to run attribution and evaluation

---
+++ scripts/run.py
@@ -0,0 +1,31 @@
+"""Run feature attribution and then evaluation."""
+
+import os
+
+from experiment_utils import parse_args
+
+
+def main():
+    """Main function."""
+    args = parse_args()
+    command_args = args.encoder_name
+    command_args += f" {args.attribution_name}"
+    command_args += f" --dataset-name {args.dataset_name}"
+    command_args += f" --explicand-size {args.explicand_size}"
+    command_args += f" --corpus-size {args.corpus_size}"
+    command_args += f" --batch-size {args.batch_size}"
+    command_args += f" --superpixel-dim {args.superpixel_dim}"
+    command_args += f" --blur-strength {args.blur_strength}"
+    if args.use_gpu:
+        command_args += " --use-gpu"
+    command_args += f" --gpu-num {args.gpu_num}"
+    command_args += f" --seed {args.seed}"
+    eval_command_args = command_args + f" --eval-superpixel-dim {args.superpixel_dim}"
+
+    os.system("python scripts/attribute.py " + command_args)
+    os.system("python scripts/eval.py" + eval_command_args)
+    os.system("python scripts/eval.py" + eval_command_args + " --take-attribution-abs")
+
+
+if __name__ == "__main__":
+    main()
