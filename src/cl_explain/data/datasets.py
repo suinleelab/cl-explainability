8498b4dc8bd7a48530b083985ff5dbbf2c4bfc77
Chris Lin <clin25@cs.washington.edu>
2022-08-19 12:58:48 -0700

    Make image subset folder varaibles general

--- src/cl_explain/data/datasets.py
+++ src/cl_explain/data/datasets.py
@@ -5,27 +5,27 @@ from typing import Dict, List, Tuple
 from torchvision.datasets import ImageFolder
 
 
-class ImagenetSubsetFolder(ImageFolder):
+class ImageSubsetFolder(ImageFolder):
     """
-    PyTorch dataset class for a subset of Imagenet classes.
+    PyTorch Dataset class for a subset of image classes.
 
     Args:
     ----
         root: Root directory path to Imagenet data folder.
-        subset_synset: List of Imagenet synsets to include.
+        subset_folders: List of subset folders to include.
         **kwargs: Other valid arguments for torchvision.datasets.ImageFolder.
     """
 
-    def __init__(self, root: str, subset_synset: List[str], **kwargs) -> None:
-        self.subset_synset = subset_synset  # Needed for super().__init__().
+    def __init__(self, root: str, subset_folders: List[str], **kwargs) -> None:
+        self.subset_folders = subset_folders  # Needed for super().__init__().
         super().__init__(root=root, **kwargs)
 
     def find_classes(self, directory: str) -> Tuple[List[str], Dict[str, int]]:
-        """Find only classes in the subset of synsets."""
+        """Find only classes in the subset folders."""
         classes = sorted(
             entry.name
             for entry in os.scandir(directory)
-            if entry.is_dir() and entry.name in self.subset_synset
+            if entry.is_dir() and entry.name in self.subset_folders
         )
         if not classes:
             raise FileNotFoundError(f"Couldn't find any class folder in {directory}.")
