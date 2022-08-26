2d8d7cabacc1089550c674655ffe568197e3cced
Chris Lin <clin25@cs.washington.edu>
2022-08-25 21:08:02 -0700

    Update screen session runs

--- scripts/run_screens.sh
+++ scripts/run_screens.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 encoder_name="simclr_x1"
-dataset_name="imagenette2"
+dataset_name="imagenet"
 
 attribution_name=${1}
 device1=${2}
 device2=${3}
 device3=${4}
+device4=${5}
 
-devices=( "${device1}" "${device2}" "${device3}" )
-explanations=( "self_weighted" "corpus" "contrastive" )
+devices=( "${device1}" "${device2}" "${device3}" "${device4}" )
+explanations=(
+    "self_weighted"
+    "contrastive_self_weighted"
+    "corpus"
+    "contrastive_corpus"
+)
 
 if [ "${attribution_name}" = "int_grad" ] \
     || [ "${attribution_name}" = "smooth_int_grad" ] \
@@ -22,7 +28,16 @@ else
     batch_size=32
 fi
 
-for i in {0..2}
+if [ "${attribution_name}" = "kernel_shap" ]
+then
+    superpixel_dim=8
+    eval_superpixel_dim=8
+else
+    superpixel_dim=1
+    eval_superpixel_dim=1
+fi
+
+for i in {0..3}
 do
     command=""
     command+="source /homes/gws/clin25/miniconda3/etc/profile.d/conda.sh;"
@@ -35,6 +50,8 @@ do
     command+=" --batch-size ${batch_size}"
     command+=" --use-gpu"
     command+=" --gpu-num ${devices[i]}"
+    command+=" --superpixel-dim ${superpixel_dim}"
+    command+=" --eval-superpixel-dim ${eval_superpixel_dim}"
     screen \
     -dmS "${encoder_name}_${explanations[i]}_${attribution_name}_${dataset_name}" \
     bash -c "${command}"
